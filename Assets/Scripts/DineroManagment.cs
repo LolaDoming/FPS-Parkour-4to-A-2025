611220e2476a93ac80253364152514460ded9c69
DOMINGUEZ LOLA <49375012@ort>
2025-05-30 09:03:04 -0300

    sdfs

---
+++ Assets/Scripts/DineroManagment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DineroManagment : MonoBehaviour
+{
+    public float playerMoney;
+    public void UpdateMoney(float amount)
+    {
+        playerMoney += amount;
+
+        if (playerMoney + amount < 0)
+        {
+            //impedir compra
+        }
+        else
+        {
+            playerMoney += amount;
+        }
+    }
+
+}
--- Assets/Scripts/MercanciaScript.cs
+++ Assets/Scripts/MercanciaScript.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class MercanciaScript : MonoBehaviour
 {
-    public int scorePoints;
+    public int precio;
+    public DineroManagment dineroManager;
 
+    private void Start()
+    {
+        dineroManager = FindObjectOfType<DineroManagment>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "Player")
+        if(collision.gameObject.CompareTag("Player"))
         {
+            //restar precio al dinero del player
+            dineroManager.UpdateMoney(precio);
             Destroy(gameObject);
-            //sumar puntos al score
         }
     }
 }
